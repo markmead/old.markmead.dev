d683e839caede84c88b10c76bba6fa1e6f708f17
markmead <itsmarkmead@gmail.com>
2021-08-16 13:16:14 +0100

    Clean up

--- pages/stack.vue
+++ pages/stack.vue
@@ -1,3 +1,11 @@
 <template>
-  <h1>stack</h1>
+  <div class="py-16 sm:py-32">
+    <section>
+      <div class="container">
+        <h1 class="font-serif text-5xl sm:text-8xl">
+          Tech Stack
+        </h1>
+      </div>
+    </section>
+  </div>
 </template>
